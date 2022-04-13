3b51f6c2f48cac234e6d95962768d9a4e2055f36
SpaW2145 <feorousse@gmail.com>
2022-04-13 11:10:43 +0200

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,6 +1,6 @@
 var speed = 1
-var x;
-var y;
+var x =50;
+var y=50;
 
 
 function setup() {
@@ -8,14 +8,12 @@ createCanvas(1080,720);
 angleMode(DEGREES);
 colorMode(HSB,360);
 background(300,0,0,200)
-x = 50
-y = 50
 }
 
 function draw() {
 
     Stroke(180,300,360);
-    ellipset(x+speed,y+speed,10,10)
+    ellipset(x,y,10,10)
 
     speed += 0.5
 

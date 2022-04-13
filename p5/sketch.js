3747faef44966943d061f3d81488a1216fc4c4ab
SpaW2145 <feorousse@gmail.com>
2022-04-13 13:32:53 +0200

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -5,20 +5,22 @@ let HUE =0;
 
 function setup() {
 createCanvas(1080,720);
-angleMode(DEGREES);
 colorMode(HSB,360);
 ball = new Balls()
+background(0,360)
 
 }
 
 function draw() {
-    background(0,150)
-    HUE += 1
+
+    HUE += 0.5
+    if(HUE == 360){HUE = 0}
 
 
     ball.move()
     ball.display()
 }
+
 
 class Balls{
     constructor(){
@@ -29,17 +31,21 @@ class Balls{
         this.yspeed = 2;
     }
     move() {
+        let ang = random(PI *2)
         this.x += this.xspeed
         this.y += this.yspeed
         if (this.x > width - this.diameter || this.x < this.diameter){
-            if(this.xspeed > 0){this.xspeed +=1.5}
-            if(this.xspeed < 0){this.xspeed -=1}
+            if(this.xspeed > 0){this.xspeed +=1.75}
+            if(this.xspeed < 0){this.xspeed -=1.5}
             this.xspeed = -this.xspeed;
+            this.xspeed = this.xspeed * cos(random(PI *2))
+
         }
         if (this.y > height - this.diameter || this.y < this.diameter) {
-            if(this.yspeed > 0){this.yspeed +=1.5}
-            if(this.yspeed < 0){this.yspeed -=1}
+            if(this.yspeed > 0){this.yspeed +=1.75}
+            if(this.yspeed < 0){this.yspeed -=1.5}
             this.yspeed = -this.yspeed ;
+            this.yspeed = this.yspeed * sin(random(PI *2))
     
         }
       }

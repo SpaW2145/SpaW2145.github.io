eb35cb1894b8485f24967289b09b1b56e8ff74e4
SpaW2145 <feorousse@gmail.com>
2022-04-13 13:20:42 +0200

    changes

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,30 +1,52 @@
-var speed = 1
-var x =500;
-var y=250;
+
+let ball
+let HUE =0;
 
 
 function setup() {
 createCanvas(1080,720);
 angleMode(DEGREES);
 colorMode(HSB,360);
-background(300,0,0)
+ball = new Balls()
+
 }
 
 function draw() {
-
-    stroke(200,300,360);
-    fill(200,300,360);
-    ellipse(x+speed,y+speed,10,10);
-    if(x >= 1080 ){speed = 1}
+    background(0,150)
+    HUE += 1
 
 
-    
-
-    
+    ball.move()
+    ball.display()
 }
-function mouseClicked() {
-    if(mouseX >=500 && mouseY>=320 && mouseX <= 580 && mouseY <= 400){
-        saveCanvas('Energy Profile', 'jpg');}
-    
 
+class Balls{
+    constructor(){
+        this.x = 500
+        this.y = 500
+        this.diameter = 10
+        this.xspeed = 2;
+        this.yspeed = 2;
+    }
+    move() {
+        this.x += this.xspeed
+        this.y += this.yspeed
+        if (this.x > width - this.diameter || this.x < this.diameter){
+            if(this.xspeed > 0){this.xspeed +=1.5}
+            if(this.xspeed < 0){this.xspeed -=1}
+            this.xspeed = -this.xspeed;
+        }
+        if (this.y > height - this.diameter || this.y < this.diameter) {
+            if(this.yspeed > 0){this.yspeed +=1.5}
+            if(this.yspeed < 0){this.yspeed -=1}
+            this.yspeed = -this.yspeed ;
+    
+        }
+      }
+    
+      display() {
+        stroke(HUE,300,360)
+        fill(HUE,300,360)
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+      }
 }

a3059b6e23ac68c526c4457c5e483482015cf0e9
SpaW2145 <feorousse@gmail.com>
2022-04-13 10:58:09 +0200

    Update sketch.js

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,51 +1,24 @@
-var angle = 0.2;
-var offset = 1080/2
-var offset2 = 720/2
-var scalar = 0.5;
-var speed = 1.2;
-var hue = 0
-var sat = 15
-var bri = 50
-var size = 7
+var speed = 1
+var x;
+var y;
 
 
 function setup() {
 createCanvas(1080,720);
 angleMode(DEGREES);
 colorMode(HSB,360);
-background(360,0,0)
-fetch('http://localhost:8408/json').then(function(res) {
-    return res.json()
-}).then(function(jsonfile) {
-    console.log(jsonfile)
-    var square = int(sqrt(jsonfile.length))+1
-    for (var i = 0; i < jsonfile.length; i++) {
-
-        var HUE =jsonfile[i].energy*100 + 145
-        var SAT =300
-        var BRI = 360
-        
-        // stroke(HUE, SAT, BRI)
-        // point(i%square, int(i/square))
-        // point(i%square+1, int(i/square))
-        // point(i%square, int(i/square+1))
-        // point(i%square+1, int(i/square+1))
-
-        var x = offset + cos(angle) * scalar 
-        var y = offset2 + sin(angle) * scalar 
-        fill(HUE, SAT, BRI);
-        noStroke();
-        ellipse(x, y, size, size);
-        angle += 70.1;
-        scalar += 0.30;
-        size += 0.02
-    }
-
-})
-
+background(300,0,0,80)
+x = 50
+y = 50
 }
 
 function draw() {
+
+    Stroke(180,100,60);
+    point(x+speed,y+speed)
+
+    speed +1
+
     
 
     

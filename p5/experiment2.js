3b418684754c980cb32bfcb8ba3840bdb716bf4b
SpaW2145 <feorousse@gmail.com>
2022-04-27 14:49:10 +0200

    dude it changed

--- p5/experiment2.js
+++ p5/experiment2.js
@@ -1,3 +1,6 @@
+var margin = 50
+var maxX = 20	
+var maxY = 15
 
 
 function setup() {
@@ -7,10 +10,35 @@ function setup() {
 }
 
 function draw() { // wird dauernd im loop aufgerufen
-	background(255);
+	background(0);
 	fill(50,50,150)
+	translate(-540,-360)
+	maxX = mouseX/20
+	maxY = mouseY/20
+	for(var x=0; x <maxX ; x+= 1){
+		for(var y=0; y<maxY;y++){
+
+
+		push()
+		translate(50*x +margin  ,50*y+margin)
+		rotateX(frameCount*random(0.01,0.02))	
+		rotateY(frameCount*random(0.01,0.02))
+		box(20)
+		pop()
+		
+		}
+	}
 	
+
+	/*push()
+	translate(margin,margin)
 	rotateX(frameCount*0.01)	
 	rotateY(frameCount*0.01)
-	box(50)
+	box(20)
+	pop()
+	push()
+	translate(margin,margin)
+	box(20)
+	pop()
+	rect(50,50,50,50)*/
 }

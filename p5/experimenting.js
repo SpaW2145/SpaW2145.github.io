4724600ab54b46ca51fe4c74c9af1ce6b4d96ed7
SpaW2145 <50777078+SpaW2145@users.noreply.github.com>
2022-05-19 08:47:46 +0200

    exper

--- p5/experimenting.js
+++ p5/experimenting.js
@@ -1,45 +1,20 @@
-var margin = 50
-var maxX = 20	
-var maxY = 15
+let x = 540
+let y = 360
 
 
 function setup() {
-	createCanvas(1080, 720,WEBGL);
+	createCanvas(1080, 720);
 	colorMode(HSB,360)
-	rectMode(CENTER);
-	//	noLoop();
-	ball = new Boxes()
+	background(0);
+
+
 }
 
 function draw() { // wird dauernd im loop aufgerufen
-	background(0);
-	fill(50,50,150)
-	translate(-540,-360)
-	maxX = mouseX/20
-	maxY = mouseY/20
-	
 
-		push()
-		translate(width/2  ,height/2)
-		ball.move()
-		ball.display()
-		/*rotateX(frameCount*0.01)	
-		rotateY(frameCount*0.01)
-		box(20)*/
-		pop()
-		
-		}
-class Boxes{
-	move(){
-		
-		rotateX(-maxY*0.05)
-		rotateY(maxX*0.05)
-	
-	}
-	display(){
-		noFill()
-		stroke(0,0,360)
-		box(400)
-	}
+	for(var i = 0; i< 100;i+20){
+		stroke(120,300,300)
+		ellipse(x+i,y+i,20)
 
+	}
 }

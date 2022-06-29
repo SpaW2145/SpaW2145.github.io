e3400ba392c2a60e9c85dda02f4d3209377dbccf
SpaW2145 <feorousse@gmail.com>
2022-06-29 11:57:34 +0200

    Update experimenting.js

--- p5/experimenting.js
+++ p5/experimenting.js
@@ -1,65 +1,82 @@
-
-
+let angle = 0;
+let z = -400;
+let offset = 100;
+let fade =0;
+let fade2 = 100;
+let angle2 =0;
+let u =400;
+var rev = false;
+var rev2 = false;
 
 function setup() {
-	createCanvas(1200, 800);
-	colorMode(HSB,360)
-	background(0);
-	eye = new Eye
+  createCanvas(800, 800, WEBGL);
+  rectMode(CENTER);
+  colorMode(HSB,100);
 
-
+  
 }
 
-function draw() { // wird dauernd im loop aufgerufen
-	
+function draw() {
+  background(0);
+  noFill();
+  strokeWeight(2);
 
-	
-	eye.display()
+    angle += 0.02;
+    angle2 -= 0.01;
+    fade += 0.1;
+    fade2 -= 0.1;
 
+    //push();
+    for (let i = 0; i < 50; i++) {
+    translate(0, 0, z);
+    rotateZ(angle);
+      
+    col = color(fade, 80,70);
+    col2 = color(fade2,80,70);
 
+    if (fade>100){fade=0;}
 
-	//curveVertex(x, y)
+    stroke(col);
+    rect(0, 0, 100, 100);
+    stroke(col2);
+    ellipse(0,0,40,40);
+    if (fade2<0){fade2=100;}
 
-}
-class Eye{
-	constructor(){
+    if (rev == false){z += 0.1}
+    
+    if (z >= width-200 || rev == true) {
+      rev = true;
+      z -= 0.1 ;
+      if(z <= -400){rev = false} 
+    }
+  }
+  //pop();
+  //push();
+  for (let x = 0; x < 50; x++) {
+    translate(0, 0, u);
+    rotateZ(angle2);
+    
+    /*col = color(fade, 80,70)
+    col2 = color(fade2,80,70)
+    if (fade>100){fade=0;}*/
+    stroke(col2);
+    rect(0, 0, 110, 110);
+    stroke(col);
+    ellipse(0,0,90,90);
+    //if (fade2<0){fade2=100;}
 
-		this.x = 80
-		this.y = 110
-		this.bri = 720
-		this.id = (random(0.01,0.022))
-
-		
-
-	}
-	display(){
-		stroke(300,300,300)
-		noFill()
-		beginShape()
-		curveVertex(80,110)// x, y
-		curveVertex(80, 110)// x,y
-		curveVertex(100, 100)//x +x/4 ,y - y/10
-		curveVertex(140, 70)//
-		curveVertex(180,50)//
-		curveVertex(220,50)//
-		curveVertex(260,70)//
-		curveVertex(300,100)//
-		curveVertex(320,110)//
-		curveVertex(320,110)//
-		endShape()
-		beginShape()
-		curveVertex(80,110)//
-		curveVertex(80, 110)//
-		curveVertex(100, 120)//
-		curveVertex(130, 135)//
-		curveVertex(180,150)//
-		curveVertex(220,150)//
-		curveVertex(270,135)//
-		curveVertex(300,120)//
-		curveVertex(320,110)//
-		curveVertex(320,110)//
-		endShape()
-	}
-	
+    //var inc = 0.01
+    if(rev2==false){
+      u -= 0.1 //+ inc
+      //inc += inc*2
+    }
+    if (u <= 0 || rev2 == true) {
+      rev2 = true;
+      u += 0.1;
+        
+      if(u >= 600){rev2 = false} 
+    }
+  }
+  //pop();
 
 }

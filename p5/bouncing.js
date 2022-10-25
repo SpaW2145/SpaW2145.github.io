860ff5c6119efaaea98ed127f4da7cf4cb6f3dee
SpaW <feorousse@gmail.com>
2022-10-25 15:37:03 +0200

    nothing really

--- p5/bouncing.js
+++ p5/bouncing.js
@@ -11,6 +11,8 @@ canvas.parent('p5sketch')
 colorMode(HSB,360);
 ball = new Balls()
 ball2 = new Balls()
+//ballsy = new Ballsy ()
+//ballsy2 = new Ballsy ()
 background(0,350)
 frameRate(120)
 rectMode(CENTER)
@@ -31,6 +33,11 @@ function draw() {
     ball.display()
     ball2.move()
     ball2.display()
+    //ballsy.move()
+    //ballsy.display()
+    //ballsy2.move()
+    //ballsy2.display()
+
 }
 
 
@@ -81,4 +88,53 @@ class Balls{
         fill(SAT,HUE,360)
         rect(this.x, this.y, this.diameter, this.diameter);
       }
+}
+
+class Ballsy{
+  constructor(){
+      this.x = 500
+      this.y = 500
+      this.diameter = 15
+      this.xspeed = 7;
+      this.yspeed = 7;
+  }
+  move() {
+      let change = random(0.5,5)*random(0.5,1.25)
+      this.x += this.xspeed
+      this.y += this.yspeed
+      if (this.x >= width - this.diameter+1 || this.x <= this.diameter-1){
+          this.xspeed = -this.xspeed;
+          if(this.xspeed > 0){this.xspeed +=change}
+          if(this.xspeed < 0){this.xspeed -=change}
+
+          
+
+          if(this.xspeed > 40){this.xspeed = random(-10,10)}
+         // this.xspeed = this.xspeed * cos(random(PI *2))
+         if(this.x > 1120 || this.x < -40){this.x=540}
+
+      }
+      if (this.y >= height - this.diameter+1 || this.y <= this.diameter-1) {
+          this.yspeed = -this.yspeed ;
+          if(this.yspeed > 0){this.yspeed +=change}
+          if(this.yspeed < 0){this.yspeed -=change}
+
+          
+
+          if(this.yspeed > 40){this.yspeed = random(-10,10)}
+        //  this.yspeed = this.yspeed * sin(random(PI *2))
+          if(this.y > 760 || this.y < -40){this.y=370}
+  
+      }
+
+
+    }
+  
+    display() {
+      HUE = map(this.xspeed,-40,40,50,360)
+      SAT = map(this.x,0,width,0,150)
+      stroke(SAT,HUE,360)
+      fill(SAT,HUE,360)
+      rect(this.x, this.y, this.diameter, this.diameter);
+    }
 }
